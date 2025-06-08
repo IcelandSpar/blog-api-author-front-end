e9ec9c520a81d8069bb5f5787145fc81512e8fb6
IcySpar <victorasielrodriguez@gmail.com>
2025-06-08 12:42:23 -0700

    feat: add login fetch req for jwt to localstorage

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Navbar from "./partials/Navbar.jsx";
 import styles from '../styles/Login.module.css';
 
@@ -6,11 +6,46 @@ const Login = () => {
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const handleSubmit = () => {
-    console.log(usernameRef.current.value)
-    console.log(passwordRef.current.value)
-  }
+  const [ loginErr, setLoginErr ] = useState(false);
 
+  const formData = new FormData()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoginErr(false);
+    let isThereLoginErr = false;
+    formData.append('username', usernameRef.current.value);
+    formData.append('password', passwordRef.current.value);
+
+    await fetch(`http://localhost:3000/login`, {
+      method: 'POST',
+      body: new URLSearchParams(formData),
+    })
+    .then((res) => {
+      if(res.ok == false) {
+        isThereLoginErr = true;
+        setLoginErr(true);
+      } else {
+        isThereLoginErr = false;
+        setLoginErr(false);
+      }
+      return res.json();
+    })
+    .then((res) => {
+      localStorage.setItem('token', res.token);
+    })
+    .catch((err) => {
+      console.error(err)
+
+    })
+    .finally(() => {
+      if(isThereLoginErr == false) {
+        window.location.href = '/';
+      } else if (isThereLoginErr == true) {
+        return null
+      }
+    })
+  }
 
   return (
     <>
@@ -19,6 +54,7 @@ const Login = () => {
         <form className={styles.loginForm}>
           <fieldset className={styles.fieldsetCont}>
             <legend>Login</legend>
+            {!loginErr ? null : <p>Username or password is incorrect, please try again.</p>}
             <div className={styles.labelAndInputCont}>
               <label htmlFor="username">Username: </label>
               <input className={styles.loginInputs} ref={usernameRef} type="text" id="username" name="username" autoComplete="true"/>
