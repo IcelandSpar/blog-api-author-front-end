76d4941f3392da0b6250f0a2a482856e82e95db8
IcySpar <victorasielrodriguez@gmail.com>
2025-06-08 13:01:54 -0700

    feat: add check if user logged in and UserContext
    if user is logged in, they will have routes
    available in navbar (conditionally rendered)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import { useState, createContext } from 'react';
+import { useState, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import UserContext from '../UserContext.jsx'
+import UserContext from './UserContext.jsx';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,9 +8,22 @@ import router from './main.jsx';
 
 function App() {
 
+  const [ isLoggedIn, setIsLoggedIn ] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    fetch(`http://localhost:3000/login/check-if-auth`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((res) => res.json())
+    .then((res) => res.isAuth ? setIsLoggedIn(true) : setIsLoggedIn(false))
+  }, [isLoggedIn])
+
   return (
     <>
-      <UserContext.Provider value='hello'>
+      <UserContext.Provider value={{isLoggedIn}}>
         <RouterProvider router={router}/>
       </UserContext.Provider>
     </>
--- src/views/partials/Navbar.jsx
+++ src/views/partials/Navbar.jsx
@@ -1,18 +1,29 @@
-import { Link } from 'react-router-dom';
-
-import styles from '../../styles/Navbar.module.css';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../../UserContext";
+import styles from "../../styles/Navbar.module.css";
 
 const Navbar = () => {
+  const { isLoggedIn } = useContext(UserContext);
+  console.log(isLoggedIn);
+
   return (
     <>
-    <nav className={styles.navBarCont}>
-    <Link to={'/'}>Home</Link>
-    <Link to={'/login'} >Login</Link>
-    <Link to={'/register'}>Register</Link>
-    <Link to={'/post-blog'}>Post Blog</Link>
-    </nav>
+      <nav className={styles.navBarCont}>
+        <Link to={"/"}>Home</Link>
+        {isLoggedIn ? (
+          <>
+            <Link to={"/post-blog"}>Post Blog</Link>
+          </>
+        ) : (
+          <>
+            <Link to={"/login"}>Login</Link>
+            <Link to={"/register"}>Register</Link>
+          </>
+        )}
+      </nav>
     </>
-  )
+  );
 };
 
 export default Navbar;
