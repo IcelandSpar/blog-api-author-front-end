0a10c0c60e0c6ad7054b679488a0fa81fe436267
IcySpar <victorasielrodriguez@gmail.com>
2025-06-14 09:24:54 -0700

    fix: page error, when user state not loaded

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -11,6 +11,24 @@ import blogProfile from '../assets/blog-profile.png';
 const Profile = () => {
   const { isAuthor } = useContext(UserContext);
 
+  // const { isAuthor, setIsAuthor } = useContext(UserContext);
+
+  // useEffect(() => {
+  //   if(isAuthor == null) {
+  //     const token = localStorage.getItem('token');
+
+  //     fetch(`http://localhost:3000/author/check-if-author`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       }
+  //     })
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setIsAuthor(res.author)
+  //     })
+  //   }
+  // }, [isAuthor, setIsAuthor])
+
   return (
     <>
     <Sidebar/>
@@ -20,11 +38,14 @@ const Profile = () => {
       <main className={styles.authorMainCont}>
       <img src={blogProfile} alt="Author Profile Picture" width='300px' height='300px'/>
       {/* Image by <a href="https://pixabay.com/users/mohamed_hassan-5229782/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3596548">Mohamed Hassan</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3596548">Pixabay</a> */}
+      {isAuthor == null ? null : (
         <section className={styles.authorInfoCont}>
-          <h1 className={styles.authorUserHeading}>{isAuthor.user.username}</h1>
-          <p className={styles.authorBio}>{isAuthor.bio}</p>
-          <p>Joined: {formatRelative(isAuthor.joined, new Date())}</p>
-        </section>
+        <h1 className={styles.authorUserHeading}>{isAuthor.user.username}</h1>
+        <p className={styles.authorBio}>{isAuthor.bio}</p>
+        <p>Joined: {formatRelative(isAuthor.joined, new Date())}</p>
+      </section>
+      )}
+
       </main>
 
     </div>
