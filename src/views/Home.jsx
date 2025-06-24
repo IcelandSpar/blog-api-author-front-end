e1eb042719c9da12ee2a6709436ce6571261afa0
IcySpar <victorasielrodriguez@gmail.com>
2025-06-24 14:57:26 -0700

    feat: add home page content and styles

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,40 @@
-// import { useContext } from "react";
-import Navbar from "./partials/Navbar";
-import Sidebar from "./partials/Sidebar";
+import Navbar from './partials/Navbar';
+import Sidebar from './partials/Sidebar.jsx';
+import styles from '../styles/Home.module.css';
+
+import frontPageBlogWord from '../assets/blog-front-page-word.png';
+import workBlog from '../assets/work_blog.png';
+import blogBookImg from '../assets/blog-book.png';
 
 const Home = () => {
-  // const {count} = useContext(UserContext);
   return (
     <>
-    <Navbar/>
     <Sidebar/>
-      <p>Home page</p>
+    <Navbar/>
+    <div className={styles.homePage}>
+      <main className={styles.homeMainCont}>
+      {/* Image by <a href="https://pixabay.com/users/thedigitalartist-202249/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1186513">Pete Linforth</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1186513">Pixabay</a> */}
+        <section className={styles.homeFrontPageSect}>
+          <img className={styles.frontPageImg} src={frontPageBlogWord} alt="front page blog" width='100%'/>
+
+          {/* <img className={styles.frontPageImg} src={frontPageBlogImg} alt="front page blog" width='100%'/> */}
+        {/* Image by <a href="https://pixabay.com/users/riona_craft_and_renovatio-939290/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2151307">riona_craft_and_renovatio</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2151307">Pixabay</a> */}
+        </section>
+        <section className={styles.imgParaCont}>
+        {/* Image by <a href="https://pixabay.com/users/megan_rexazin_conde-6742250/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5000692">Megan Rexazin Conde</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5000692">Pixabay</a> */}
+          <p>Read blogs from billions of auhors!</p>
+          <img className={styles.firstContImg} src={blogBookImg} alt="blog book" width='200px'/>
+
+        </section>
+        <section className={styles.imgParaCont}>
+          {/* Image by <a href="https://pixabay.com/users/arivleone-14275976/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4997565">Arivle One</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4997565">Pixabay</a> */}
+          <img className={styles.firstContImg} src={workBlog} alt="bloggers" width='200px'/>
+          <p>Read blogs from our authors!</p>
+        </section>
+      </main>
+    </div>
     </>
-  );
+  )
 };
 
 export default Home;
