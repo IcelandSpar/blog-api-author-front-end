51a385a084b178f7cf9869d1548b9c4e2c902af9
IcySpar <victorasielrodriguez@gmail.com>
2025-07-08 12:45:53 -0700

    feat: add err messages to register and login forms
    forms will be validated serverside and if form
    submission fails, messages are posted on form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,71 +1,113 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 import Navbar from "./partials/Navbar.jsx";
-import Sidebar from './partials/Sidebar.jsx';
-import styles from '../styles/Login.module.css';
+import Sidebar from "./partials/Sidebar.jsx";
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const [ loginErr, setLoginErr ] = useState(false);
+  const [loginErr, setLoginErr] = useState(false);
+  const [errMsgs, setErrMsgs] = useState([]);
 
-  const formData = new FormData()
+  const formData = new FormData();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setErrMsgs([]);
     setLoginErr(false);
     let isThereLoginErr = false;
-    formData.append('username', usernameRef.current.value);
-    formData.append('password', passwordRef.current.value);
+    formData.append("username", usernameRef.current.value);
+    formData.append("password", passwordRef.current.value);
 
     await fetch(`http://localhost:3000/login`, {
-      method: 'POST',
+      method: "POST",
       body: new URLSearchParams(formData),
     })
-    .then((res) => {
-      if(res.ok == false) {
-        isThereLoginErr = true;
-        setLoginErr(true);
-      } else {
-        isThereLoginErr = false;
-        setLoginErr(false);
-      }
-      return res.json();
-    })
-    .then((res) => {
-      localStorage.setItem('token', res.token);
-    })
-    .catch((err) => {
-      console.error(err)
-
-    })
-    .finally(() => {
-      if(isThereLoginErr == false) {
-        window.location.href = '/';
-      } else if (isThereLoginErr == true) {
-        return null
-      }
-    })
-  }
+      .then((res) => {
+        if (res.ok == false) {
+          isThereLoginErr = true;
+          setLoginErr(true);
+        } else {
+          isThereLoginErr = false;
+          setLoginErr(false);
+        }
+        return res.json();
+      })
+      .then((res) => {
+        if (res.token) {
+          localStorage.setItem("token", res.token);
+        } else if (res.errors) {
+          setErrMsgs(res.errors);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        if (isThereLoginErr == false) {
+          window.location.href = "/";
+        } else if (isThereLoginErr == true) {
+          return null;
+        }
+      });
+  };
 
   return (
     <>
-      <Sidebar/>
+      <Sidebar />
       <Navbar />
       <main className={styles.loginMainCont}>
         <form onSubmit={handleSubmit} className={styles.loginForm}>
           <fieldset className={styles.fieldsetCont}>
             <legend className={styles.loginLegend}>Login</legend>
-            {!loginErr ? null : <p>Username or password is incorrect, please try again.</p>}
+            {!errMsgs ? null : (
+              <ul>
+                {errMsgs.map((errorMsg, indx) => {
+                  return (
+                    <li className={styles.validationMsgs} key={indx}>
+                      {errorMsg.msg}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            {!loginErr ? null : (
+              <p>Username or password is incorrect, please try again.</p>
+            )}
             <div className={styles.labelAndInputCont}>
-              <label className={styles.inputLabels} htmlFor="username">Username: </label>
-              <input className={styles.loginInputs} ref={usernameRef} type="text" id="username" name="username" autoComplete="true" autoFocus/>
+              <label className={styles.inputLabels} htmlFor="username">
+                Username:{" "}
+              </label>
+              <input
+                className={styles.loginInputs}
+                ref={usernameRef}
+                type="text"
+                id="username"
+                name="username"
+                autoComplete="true"
+                autoFocus
+              />
             </div>
             <div className={styles.labelAndInputCont}>
-              <label className={styles.inputLabels} htmlFor="password">Password: </label>
-              <input className={styles.loginInputs} ref={passwordRef} type="password" id="password" name="password"/>
+              <label className={styles.inputLabels} htmlFor="password">
+                Password:{" "}
+              </label>
+              <input
+                className={styles.loginInputs}
+                ref={passwordRef}
+                type="password"
+                id="password"
+                name="password"
+              />
             </div>
-            <button type="submit" onClick={handleSubmit} className={styles.loginBtn}>Login</button>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className={styles.loginBtn}
+            >
+              Login
+            </button>
           </fieldset>
         </form>
       </main>
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
 import Navbar from "./partials/Navbar.jsx";
 import Sidebar from "./partials/Sidebar.jsx";
-import styles from '../styles/Register.module.css';
+import styles from "../styles/Register.module.css";
 
 const Register = () => {
   const usernameInput = useRef(null);
@@ -10,76 +10,120 @@ const Register = () => {
   const checkboxInput = useRef(null);
   const bioInput = useRef(null);
 
-  const [ isBioOpen, setIsBioOpen ] = useState(false);
-  const [ registerErr, setRegisterErr ] = useState(false);
-  const [ errMsg, setErrMsg ] = useState('');
+  const [isBioOpen, setIsBioOpen] = useState(false);
+  const [registerErr, setRegisterErr] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [errMsgs, setErrMsgs] = useState([]);
 
   const handleCheck = (e) => {
-    setIsBioOpen(e.target.checked)
-  }
+    setIsBioOpen(e.target.checked);
+  };
 
   const handleRegister = (e) => {
     e.preventDefault();
+    setErrMsgs([]);
     setRegisterErr(false);
     const formData = new FormData();
-    formData.append('username', usernameInput.current.value)
-    formData.append('password', passwordInput.current.value)
-    formData.append('author', checkboxInput.current.checked)
-    if(checkboxInput.current.checked) {
-      formData.append('bio', bioInput.current.value);
+    formData.append("username", usernameInput.current.value);
+    formData.append("password", passwordInput.current.value);
+    formData.append("author", checkboxInput.current.checked);
+    if (checkboxInput.current.checked) {
+      formData.append("bio", bioInput.current.value);
     }
     fetch(`http://localhost:3000/register`, {
-      method: 'POST',
+      method: "POST",
       body: new URLSearchParams(formData),
     })
-    .then((res) => {
-      if(!res.ok) {
+      .then((res) => {
+        if (!res.ok) {
+          setRegisterErr(true);
+        } else if (res.ok) {
+          window.location = "/";
+        }
+        return res.json();
+      })
+      .then((res) => {
+        if (res.errors) {
+          setErrMsgs(res.errors);
+        }
+        if (res.message) {
+          setErrMsg(res.message);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
         setRegisterErr(true);
-      } else if(res.ok) {
-        window.location = '/'
-      }
-      return res.json();
-    })
-    .then((res) => {
-      if(res.message) {
-        setErrMsg(res.message);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      setRegisterErr(true);
-      setErrMsg('Something went wrong...');
-
-    })
-  }
+        setErrMsg("Something went wrong...");
+      });
+  };
 
   return (
     <>
-      <Sidebar/>
+      <Sidebar />
       <Navbar />
       <main className={styles.registerMainCont}>
         <form onSubmit={handleRegister}>
           <fieldset className={styles.fieldsetCont}>
             <legend className={styles.registerLegend}>Register</legend>
-            {!registerErr ? null : (
-              <p>{errMsg}</p>
+            {!errMsgs ? null : (
+              <ul>
+                {errMsgs.map((errorMsg, indx) => {
+                  return (
+                    <li className={styles.validationMsgs} key={indx}>
+                      {errorMsg.msg}
+                    </li>
+                  );
+                })}
+              </ul>
             )}
+            {!registerErr ? null : <p>{errMsg}</p>}
             <div className={styles.labelAndInputCont}>
-              <label className={styles.inputLabels} htmlFor="username">Username: </label>
-              <input ref={usernameInput} className={styles.registerInputs} type="text" id="username" name="username" autoFocus required/>
+              <label className={styles.inputLabels} htmlFor="username">
+                Username:{" "}
+              </label>
+              <input
+                ref={usernameInput}
+                className={styles.registerInputs}
+                type="text"
+                id="username"
+                name="username"
+                autoFocus
+              />
             </div>
             <div className={styles.labelAndInputCont}>
-              <label className={styles.inputLabels} htmlFor="password">Password: </label>
-              <input ref={passwordInput} className={styles.registerInputs} type="password" id="password" name="password" required/>
+              <label className={styles.inputLabels} htmlFor="password">
+                Password:{" "}
+              </label>
+              <input
+                ref={passwordInput}
+                className={styles.registerInputs}
+                type="password"
+                id="password"
+                name="password"
+              />
             </div>
-              {isBioOpen ? (
+            {isBioOpen ? (
               <div className={styles.labelAndInputCont}>
-                <label className={styles.inputLabels} htmlFor="bio">Author Bio:</label>
-                <textarea ref={bioInput} name="bio" id="bio" required></textarea>
+                <label className={styles.inputLabels} htmlFor="bio">
+                  Author Bio:
+                </label>
+                <textarea
+                  ref={bioInput}
+                  name="bio"
+                  id="bio"
+                  
+                ></textarea>
               </div>
             ) : null}
             <div className={styles.checkBoxCont}>
-              <input onClick={handleCheck} type="checkbox" className={styles.registerInputs} ref={checkboxInput} id='author' name='author'/>
+              <input
+                onClick={handleCheck}
+                type="checkbox"
+                className={styles.registerInputs}
+                ref={checkboxInput}
+                id="author"
+                name="author"
+              />
               <label htmlFor="author">Become an Author</label>
             </div>
 
